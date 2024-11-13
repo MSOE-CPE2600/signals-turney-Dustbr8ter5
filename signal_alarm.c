c49747971d921b0c59bfd56d34dda971a2a05a20
Vincent Vassallo <vassallov@msoe.edu>
2024-11-13 10:50:30 -0600

    Signal Alarm Done

--- signal_alarm.c
+++ signal_alarm.c
@@ -1,6 +1,25 @@
 /**
- * File: 
- * Modified by:
+ * File: signal_alarm.c
+ * Modified by: Vincent Vassallo
  * 
- * Brief summary of program:
+ * Brief summary of program: The program waits for a 5 second alarm in
+ * an infinite loop, it then catches the alarm signal and exits
  */
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+void handle() {
+    printf("Alarm recieved...\n");
+    exit(1);
+}
+
+int main() {
+    signal(SIGALRM, handle);
+    alarm(5);
+        int i = 0;
+    while(1) {
+        i++;
+    }
+    return i;
+}
