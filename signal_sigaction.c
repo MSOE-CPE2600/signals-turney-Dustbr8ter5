5efed45ae912870083aede99dbcb5dafc62d3f68
Vincent Vassallo <vassallov@msoe.edu>
2024-11-13 14:35:45 -0600

    Signal Sigaction Done

--- signal_sigaction.c
+++ signal_sigaction.c
@@ -1,8 +1,36 @@
 /**
- * File: 
- * Modified by:
+ * File: signal_sigaction.c
+ * Modified by: Vincent Vassallo
  * 
  * Brief summary of program:
  */
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+void handler(int signo, siginfo_t *info, void *context) {
+    int pid = info->si_pid;
+    printf("Signal origin pid: %d\n", pid);
+}
+int main() {
+    int pid = fork();
+    if(pid == 0) {
+        //Child
+        kill(getppid(), SIGUSR1);
+        printf("Child Done\n");
+
+    } else {
+        struct sigaction new_action;
+        new_action.sa_sigaction = handler;
+        new_action.sa_flags = SA_SIGINFO;
+        sigaction(SIGUSR1, &new_action, NULL);
+        wait(NULL);
+        printf("Parent Done\n");
+        
+    }
+    return 0;
+}
 
 
