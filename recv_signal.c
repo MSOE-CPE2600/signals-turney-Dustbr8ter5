c644ded6165a6ab2d612fdf1e4876871eff8fc03
Vincent Vassallo <vassallov@msoe.edu>
2024-11-14 00:18:28 -0600

    recv_signal done

--- recv_signal.c
+++ recv_signal.c
@@ -1,6 +1,34 @@
 /**
- * File: 
- * Modified by:
+ * File: recv_signal
+ * Modified by: Vincent Vassallo
  * 
  * Brief summary of program:
  */
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+void handler(int signo, siginfo_t *info, void *context) {
+    //Get sival
+    
+    printf("Signal value: %d\n", info->si_int);
+    //"Return"
+    exit(1);
+}
+
+int main() {
+    
+    //Create struct to hold data
+    struct sigaction new_action;
+    //Set the struct to the handler
+    new_action.sa_sigaction = handler;
+    //Set the flag to save the data
+    new_action.sa_flags = SA_SIGINFO;
+    //Register for signal
+    sigaction(SIGUSR1, &new_action, NULL);
+    //wait for the child to send signal
+    while(1);
+    return 0;
+}
--- signal_sigaction.c
+++ signal_sigaction.c
@@ -2,7 +2,8 @@
  * File: signal_sigaction.c
  * Modified by: Vincent Vassallo
  * 
- * Brief summary of program:
+ * Brief summary of program: The program forks and uses a child to pass
+ * the SIGUSR1 signal to the parent which then is caught by the handler
  */
 #include <signal.h>
 #include <unistd.h>
@@ -18,14 +19,20 @@ int main() {
     int pid = fork();
     if(pid == 0) {
         //Child
+        //Send Signal
         kill(getppid(), SIGUSR1);
         printf("Child Done\n");
 
     } else {
+        //Create struct to hold data
         struct sigaction new_action;
+        //Set the struct to the handler
         new_action.sa_sigaction = handler;
+        //Set the flag to save the data
         new_action.sa_flags = SA_SIGINFO;
+        //Register for signal
         sigaction(SIGUSR1, &new_action, NULL);
+        //wait for the child to send signal
         wait(NULL);
         printf("Parent Done\n");
         
